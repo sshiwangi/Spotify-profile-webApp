6589bd27d1cb03cc9c0eb3e039936d722100b3e9
Shiwangi Kumari <sshiwangi770@gmail.com>
2023-04-13 13:36:31 +0530

    added button

---
+++ src/components/Button/Button.js
@@ -0,0 +1,8 @@
+import React from "react"
+import button from "./button.css"
+
+export default function Button () {
+    return (
+        <div className="spotify-btn">sign up with spotify</div>
+    )
+}
